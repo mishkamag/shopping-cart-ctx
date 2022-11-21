aceafc5d55667f1dc30727b4c374bd44d3a34961
mishkamag <m93.maglaperidze@gmail.com>
2022-11-22 00:55:23 +0400

    some changes

--- src/components/Ui/SingleProduct.js
+++ src/components/Ui/SingleProduct.js
@@ -1,18 +1,18 @@
 import classes from "./SingleProduct.module.css";
 
 const SingleProduct = (props) => {
-  const { img, title, price, category } = props;
+  const { img, title, price } = props;
   return (
-    <div className={classes.container}>
+    <section className={classes.container}>
       <div className={classes.card}>
         <img src={img} alt={title} />
 
         <h3>{title}</h3>
-        <h3>{category}</h3>
         <span>${price}</span>
+
         <button>Add to cart</button>
       </div>
-    </div>
+    </section>
   );
 };
 
