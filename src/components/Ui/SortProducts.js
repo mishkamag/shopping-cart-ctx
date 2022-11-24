8f57d0880d76ab22c2fdd8aec4b013eddc1a8ef1
mishkamag <m93.maglaperidze@gmail.com>
2022-11-24 23:59:35 +0400

    have bug of select

--- src/components/Ui/SortProducts.js
+++ src/components/Ui/SortProducts.js
@@ -1,17 +1,49 @@
-const SortProducts = ({ productsArr }) => {
-  console.log(productsArr);
-  const lowestFirst = productsArr.sort((a, b) => a.price - b.price);
-  const highestFirst = productsArr.sort((a, b) => b.price - a.price);
+import { useState } from "react";
+import SingleProduct from "./SingleProduct";
 
-  console.log(lowestFirst);
-  // console.log(highestFirst);
+const SortProducts = ({ productsArr }) => {
+  const [selectValue, setSelectValue] = useState("");
+
+  const lowestFirst = [...productsArr]
+    .sort((a, b) => a.price - b.price)
+    .map((prod) => (
+      <SingleProduct
+        id={prod.id}
+        key={prod.key}
+        img={prod.img}
+        title={prod.title}
+        price={prod.price}
+      />
+    ));
+
+  const highestFirst = [...productsArr]
+    .sort((a, b) => b.price - a.price)
+    .map((prod2) => (
+      <div>
+        <SingleProduct
+          id={prod2.id}
+          key={prod2.key}
+          img={prod2.img}
+          title={prod2.title}
+          price={prod2.price}
+        />
+      </div>
+    ));
+
+  const selectChangeHandler = (event) => {
+    setSelectValue(event.target.value);
+  };
 
   return (
     <div>
-      <select name="price">
-        <option value="" disabled selected>
+      <select
+        name="price"
+        onChange={selectChangeHandler}
+        defaultValue={selectValue}
+      >
+        {/* <option value="" disabled selected>
           Filter Products
-        </option>
+        </option> */}
         <option value={lowestFirst}>Price (lowest first)</option>
         <option value={highestFirst}>Price (highest first)</option>
       </select>
