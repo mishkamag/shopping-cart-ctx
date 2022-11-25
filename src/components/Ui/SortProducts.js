22aa2f2678efb7a718eea0aa3f7c4f53e3a66db7
mishkamag <m93.maglaperidze@gmail.com>
2022-11-25 22:39:06 +0400

    now more clean code

--- src/components/Ui/SortProducts.js
+++ src/components/Ui/SortProducts.js
@@ -9,40 +9,10 @@ const SortProducts = ({ productsArr }) => {
   const [selectValue, setSelectValue] = useState("");
 
   const lowestFirst = [...productsArr].sort((a, b) => a.price - b.price);
-
-  const lowFilter = (
-    <Card>
-      {lowestFirst.map((prod) => (
-        <SingleProduct
-          id={prod.id}
-          key={prod.id}
-          img={prod.image}
-          title={prod.title}
-          price={prod.price}
-          manageCart={ctx.addToCart}
-          managing="Adding"
-        />
-      ))}
-    </Card>
-  );
-
   const highestFirst = [...productsArr].sort((a, b) => b.price - a.price);
 
-  const highestFilter = (
-    <Card>
-      {highestFirst.map((prod2) => (
-        <SingleProduct
-          id={prod2.id}
-          key={prod2.id}
-          img={prod2.image}
-          title={prod2.title}
-          price={prod2.price}
-          manageCart={ctx.addToCart}
-          managing="Adding"
-        />
-      ))}
-    </Card>
-  );
+  let checkValue =
+    selectValue === "Price (highest first)" ? highestFirst : lowestFirst;
 
   const selectChangeHandler = (event) => {
     setSelectValue(event.target.value);
@@ -62,7 +32,22 @@ const SortProducts = ({ productsArr }) => {
         <option>Price (highest first)</option>
         <option>Price (lowest first)</option>
       </select>
-      {selectValue === "Price (highest first)" ? highestFilter : lowFilter}
+
+      {
+        <Card>
+          {checkValue.map((prod) => (
+            <SingleProduct
+              id={prod.id}
+              key={prod.id}
+              img={prod.image}
+              title={prod.title}
+              price={prod.price}
+              manageCart={ctx.addToCart}
+              managing="Adding"
+            />
+          ))}
+        </Card>
+      }
     </div>
   );
 };
