a3a9438c1c133ae55f11193f9658199ff93c8f51
mishkamag <m93.maglaperidze@gmail.com>
2022-11-26 00:00:01 +0400

    add shoppin cart icon

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,7 @@ import classes from "./Header.module.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import ShoppingCartContext from "../../store/ShoppingCartCtx";
+import { FiShoppingCart } from "react-icons/fi";
 
 const Header = () => {
   const ctx = useContext(ShoppingCartContext);
@@ -12,10 +13,15 @@ const Header = () => {
       <span className={classes.header}>Great Shop</span>
       <ul className={classes.nav}>
         <li>
-          <Link to="/">Home</Link>
+          <Link className={classes.home} to="/">
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/cart">Cart {list}</Link>
+          <Link to="/cart">
+            <FiShoppingCart size={28} />
+            {list === 0 ? null : list}
+          </Link>
         </li>
       </ul>
     </div>
