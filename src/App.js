47c60bccd80a3d53c9ba8b52d4306914305b7d7d
mishkamag <m93.maglaperidze@gmail.com>
2022-12-12 21:39:04 +0400

    finished project

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="signin" element={<SignIn />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </div>
   );
